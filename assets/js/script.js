9b70693edfe7cb61e260c54426df18249dd199a7
jmanager25 <jmanager25@hotmail.com>
2022-11-24 22:08:39 +0000

    Display addition questions to users

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,19 +8,29 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
+
 })
 
 /**
  * The main game "loop", called when the script is firt loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = math.floor(math.ramdom() * 25) + 1
     let num2 = math.floor(math.ramdom() * 25) + 1
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2)
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}.Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -39,8 +49,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion () {
-
+function displayAdditionQuestion (operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion() {
